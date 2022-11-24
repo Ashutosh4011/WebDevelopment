26ad84b6605de80b449b3362304589a34a4775cc
Ashutosh4011 <mashukla198@gmail.com>
2022-11-24 14:31:07 +0530

    Web Development

---
+++ MediaQueryProject/MediaQ.js
@@ -0,0 +1,8 @@
+Window.onresize=screen;
+window.onload=screen;
+
+function screen(){
+    myWidth=window.innerWidth;
+
+    document.getElementById('size').innerHTML="Width : " + myWidth + "px" ;
+}
